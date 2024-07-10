59e78c6dd55cdda1c8e42594742d5b560429f164
Armando Lustosa <115752285+armandolustosa@users.noreply.github.com>
2024-07-10 08:01:26 -0300

    Interligando a página Sobre

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,99 +1,126 @@
 <template>
-    <nav class="navbar bg-body fixed menu">
-        <div class="container-fluid d-flex justify-content-start">
-            <button data-bs-toggle="offcanvas" class="navbar-brand border-0 bg-body" data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar">
-                <i class="bi bi-list text-white"></i>
-            </button>
-            <a class="navbar-brand d-flex justify-content-start text-white" href="#">Meus Chamados</a>
-            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Opções</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <div class="user">
-                        <img src="@/assets/logo-amigos.png" alt="" width="60px" />
-                        <div class="dados">
-                            <span>Marcos André De Andrade Amaro</span>
-                            <span>annndre2016@gmail.com</span>
-                        </div>
-                    </div>
-                    <ul class="navbar-nav border-top">
-                        <li class="nav-item d-flex justify-content-start">
-                            <i class="bi bi-person-lock"></i>
-                            <a class="nav-link" aria-current="page" href="#">Alterar Senha</a>
-                        </li>
-                        <li class="nav-item d-flex justify-content-start">
-                            <i class="bi bi-info-circle-fill"></i>
-                            <a class="nav-link" href="#">Sobre</a>
-                        </li>
-                        <li class="nav-item d-flex justify-content-start">
-                            <i class="bi bi-door-open-fill"></i>
-                            <a class="nav-link" href="#">Sair</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  <nav class="navbar bg-body fixed menu">
+    <div class="container-fluid d-flex justify-content-start">
+      <button
+        data-bs-toggle="offcanvas"
+        class="navbar-brand border-0 bg-body"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar"
+      >
+        <i class="bi bi-list text-white"></i>
+      </button>
+      <a class="navbar-brand d-flex justify-content-start text-white" href="#"
+        >Meus Chamados</a
+      >
+      <div
+        class="offcanvas offcanvas-start"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Opções</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
         </div>
-    </nav>
+        <div class="offcanvas-body">
+          <div class="user">
+            <img src="@/assets/logo-amigos.png" alt="" width="60px" />
+            <div class="dados">
+              <span>Marcos André De Andrade Amaro</span>
+              <span>annndre2016@gmail.com</span>
+            </div>
+          </div>
+          <ul class="navbar-nav border-top">
+            <li class="nav-item d-flex justify-content-start">
+              <i class="bi bi-person-lock"></i>
+              <a class="nav-link" aria-current="page" href="#">Alterar Senha</a>
+            </li>
+            <li class="nav-item d-flex justify-content-start">
+              <i class="bi bi-info-circle-fill"></i>
+              <a class="nav-link" href="#" @click="abrirSobre">Sobre</a>
+            </li>
+            <li class="nav-item d-flex justify-content-start">
+              <i class="bi bi-door-open-fill"></i>
+              <a class="nav-link" href="#">Sair</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
+
+<script>
+export default {
+  name: "NavBarComponent",
+  methods: {
+    abrirSobre() {
+      this.$router.push("/sobre");
+    },
+  },
+};
+</script>
+
 <style scoped>
 .background {
-    background-color: #6ea8fe;
-    height: 100vh;
-    display: flex;
-    margin: 0px;
-    flex-direction: column;
-    justify-content: space-between;
+  background-color: #6ea8fe;
+  height: 100vh;
+  display: flex;
+  margin: 0px;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 .bg-body {
-    --bs-bg-opacity: 0;
+  --bs-bg-opacity: 0;
 }
 
 .menu {
-    color: rgb(255, 255, 255);
-    --bs-bg-opacity: 0;
-    background-color: #6ea8fe;
-    font-family: Poppins;
-    font-size: 16px;
-    font-weight: 600;
-    line-height: 40px;
-    text-align: center;
+  color: rgb(255, 255, 255);
+  --bs-bg-opacity: 0;
+  background-color: #6ea8fe;
+  font-family: Poppins;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 40px;
+  text-align: center;
 }
 
 .offcanvas {
-    max-width: 70%;
+  max-width: 70%;
 }
-.nav-item{
-    align-items: center;
-    gap: 5px;
+.nav-item {
+  align-items: center;
+  gap: 5px;
 }
-.nav-item{
-    color: #555450;
+.nav-item {
+  color: #555450;
 }
 .dados span {
-    font-family: Poppins;
-    font-size: 12px;
-    font-weight: 600;
-    line-height: 35px;
-    text-align: center;
+  font-family: Poppins;
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 35px;
+  text-align: center;
 }
-.user{
-    display: flex;
-    color: #767575;
-    border-bottom: solid 1px #767575B0;
-    border-width: 100%;
-    flex-direction: column;
-    align-items: center;
+.user {
+  display: flex;
+  color: #767575;
+  border-bottom: solid 1px #767575b0;
+  border-width: 100%;
+  flex-direction: column;
+  align-items: center;
 }
 .offcanvas-body {
-    gap: 30px;
+  gap: 30px;
 }
-.user img{
-    border-radius: 50%;
-    background-color: black;
+.user img {
+  border-radius: 50%;
+  background-color: black;
 }
 </style>
--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -1,52 +1,65 @@
 <template>
-  <nav class="navbar">
-    <div class="container-fluid">
-      <h1 class="navbar-brand">Sobre</h1>
-      <i class="bi bi-x"></i>
-    </div>
-  </nav>
-  <section class="text-center">
-    <h2>Startup Do Bem</h2>
-    <img
-      src="../assets/imagens/startup.jpeg"
-      class="rounded"
-      alt="Integrantes da Startup"
-    />
-    <p class="mt-4">
-      <strong>A Startup do Bem</strong> é uma iniciativa inovadora da ONG Amigos
-      do Bem, no sertão nordestino. O projeto, composto por 10 jovens
-      capacitados em tecnologia, visa desenvolver soluções tecnológicas
-      melhorando a infraestrutura e os serviços internos.
-    </p>
-  </section>
-  <section>
-    <h3>Descrição</h3>
-    <p>
-      Este aplicativo facilita a abertura de chamados de serviços internos na
-      unidade dos Amigos do Bem no Ceará, melhorando a comunicação e a gestão de
-      demandas.
-    </p>
-    <h3>Parceiros</h3>
-    <p>
-      O projeto conta com o apoio da ONG Amigos do Bem, que oferece recursos e
-      suporte essenciais. Sob a mentoria dos desenvolvedores Vitor Siqueira e
-      Danilo, estamos comprometidos em criar um impacto positivo na vida das
-      pessoas da região.
-    </p>
-    <h3>Contato</h3>
-    <p>
-      Para mais informações, entre em contato pelo e-mail
-      <a href="mailto:contato@startupdobem.org">contato@startupdobem.org</a>
-    </p>
-    <h3>Versão</h3>
-    <p class="text-center">Versão 1.0.0</p>
-  </section>
+  <div class="main">
+    <nav class="navbar">
+      <div class="container-fluid">
+        <h1 class="navbar-brand">Sobre</h1>
+        <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+            @click="fecharPagina"
+          ></button>
+      </div>
+    </nav>
+    <section class="text-center">
+      <h2>Startup Do Bem</h2>
+      <img
+        src="../assets/imagens/startup.jpeg"
+        class="rounded"
+        alt="Integrantes da Startup"
+      />
+      <p class="mt-4">
+        <strong>A Startup do Bem</strong> é uma iniciativa inovadora da ONG
+        Amigos do Bem, no sertão nordestino. O projeto, composto por 10 jovens
+        capacitados em tecnologia, visa desenvolver soluções tecnológicas
+        melhorando a infraestrutura e os serviços internos.
+      </p>
+    </section>
+    <section>
+      <h3>Descrição</h3>
+      <p>
+        Este aplicativo facilita a abertura de chamados de serviços internos na
+        unidade dos Amigos do Bem no Ceará, melhorando a comunicação e a gestão
+        de demandas.
+      </p>
+      <h3>Parceiros</h3>
+      <p>
+        O projeto conta com o apoio da ONG Amigos do Bem, que oferece recursos e
+        suporte essenciais. Sob a mentoria dos desenvolvedores Vitor Siqueira e
+        Danilo, estamos comprometidos em criar um impacto positivo na vida das
+        pessoas da região.
+      </p>
+      <h3>Contato</h3>
+      <p>
+        Para mais informações, entre em contato pelo e-mail
+        <a href="mailto:contato@startupdobem.org">contato@startupdobem.org</a>
+      </p>
+      <h3>Versão</h3>
+      <p class="text-center">Versão 1.0.0</p>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
   name: "Sobre",
   components: {},
+  methods: {
+    fecharPagina() {
+      this.$router.push("/chamados");
+    }
+  }
 };
 </script>
 
@@ -80,8 +93,7 @@ p {
 img {
   width: 200px;
 }
-.bi-x {
-  font-size: 30px;
+button {
   padding: 20px;
 }
 </style>
